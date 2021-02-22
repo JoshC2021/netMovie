340991f2777c38fa1b27e6b18bec8b6834fc78ed
Josh <josh_2021@yahoo.com>
2021-02-22 08:55:04 -0500

    initial commit

---
+++ netMovieMVC/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace netMovieMVC.Models
+{
+    public enum Genre
+    {
+        Drama,
+        Action,
+        Horror,
+        Romance,
+        Comedy
+    }
+    public class Movie
+    {
+        public int ID { get; set; } = 0;
+        public string Title { get; set; }
+        public Genre Category { get; set; }
+        public DateTime Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+    }
+}
